b87c187ab7bc82bdc96022b818f4688a221dda65
Srikar Manthatti <srikarmanthatti@gmail.com>
2025-04-26 09:31:03 +1000

    fixed issue in imdb fetching

--- nuploaders/imdb_url_fetcher.py
+++ nuploaders/imdb_url_fetcher.py
@@ -1,16 +1,20 @@
 import requests
+import os
 from typing import Optional #for type hinting
 from dataclasses import dataclass
+from nuploaders import CONFIG, log
 
 
-class IMDBFetcher():
+
+class IMDBFetcher(): #make a cache folder to load url quickly instead of hitting api everytime
     def __init__(self, base_link: str):
         self.base_link = base_link
         self.headers = {'User-Agent': 'Mozilla/5.0'} #using it to mimic browser, else IMDB return 403 client error
         self.ref = "?ref_=fn_all_ttl_1"
     
     def generate_link(self, titleid: str) -> str:
-        return _validate_imdb_url(f"{self.base_link}/title/{titleid}/{self.ref}")
+        log.info("Validating URL for titleid: %s", titleid)
+        return self._validate_imdb_url(f"{self.base_link}/title/{titleid}/{self.ref}")
     
     def _validate_imdb_url(self, link: str) -> str:
         response = requests.get(link, headers = self.headers)
@@ -23,6 +27,7 @@ class Movie():
     """This is a data class which I am using to create a data structure for movie"""
     title: str
     runtime: str
+    mycategory: str
     genre: str
     ratings: list
     imdbrating: float
@@ -37,28 +42,30 @@ class OMDBClient():
         self.api_key = os.getenv("OMDB_API_KEY") #api_key
         self.base_link = base_link
     
-    def fetch_movie(self, movie_name) -> Optional[Movie]:
+    def fetch_movie(self, movie_name, mycategory) -> Optional[Movie]:
         params = {"t": movie_name, "apikey": self.api_key}
 
         try:
             response = requests.get(self.base_link, params = params, timeout = 10)
+            log.info("Response received for movie %s ", movie_name)
             response.raise_for_status()
         except requests.RequestException as re:
-            log.Error("Error in fetching information for movie: %s", self.movie_name)
+            log.Error("Error in fetching information for movie: %s", movie_name)
             return None
         
-        imdb_obj = IMDBFetcher() #pass config value
+        imdb_obj = IMDBFetcher(CONFIG["imdb"]["base_link"]) #pass config value
         data = response.json()
         # check EXAMPLES at https://www.omdbapi.com/ to get sample response
         if data.get("Response") == "True":
             return Movie(
                 title = data.get("Title", "N/A"),
                 runtime = data.get("Runtime", "N/A"),
+                mycategory = mycategory,
                 genre =  data.get("Genre", "N/A"),
                 ratings =  data.get("Ratings", []), 
                 imdbrating =  data.get("imdbRating","N/A"),
                 imdbid =  data.get("imdbID","N/A"),
-                imdblink = imdbobj.generate_link(imdbid),
+                imdblink = imdb_obj.generate_link(data.get("imdbID","N/A")),
             )
         
 
--- nuploaders/main.py
+++ nuploaders/main.py
@@ -7,9 +7,33 @@ def main():
     rdobj = file_reader.ReadActor()
     df_obj = rdobj.read_file(CONFIG["raw_files"]["movies"]["path"])
     df = df_obj.read()
-    log.info("just priting testing")
-    log.info(df.head())
 
-if __name__ == "__main__":
+    cleaned_df = clean_movies_dataframe(df)
+    cleaned_df.to_csv("output.csv")
+    log.info(cleaned_df)
+
+def clean_movies_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """THe sheet which we got have myowncategory as columns so we will change that as a column"""
+    #unpivoting the df to make column names as myown category
+    unpivot_df = df.melt(var_name='mycategory', value_name='name')
+    unpivot_df = unpivot_df[['name', 'mycategory']]
+
+    omdb_obj = imdb_url_fetcher.OMDBClient(CONFIG["omdb"]["base_link"])
+    for index, row in unpivot_df.iterrows():
+        log.info("Info for movie %s", row['name'])
+        movie_obj = omdb_obj.fetch_movie(row['name'], row['mycategory'])
+        unpivot_df.loc[index, ['title','runtime','enrich_mycategory','genre','ratings','imdbrating','imdbid','imdblink']] = [
+        movie_obj.title,
+        movie_obj.runtime,
+        movie_obj.mycategory,
+        movie_obj.genre,
+        str(movie_obj.ratings),
+        movie_obj.imdbrating,
+        movie_obj.imdbid,
+        movie_obj.imdblink
+    ]
+    return unpivot_df
+
+
+if __name__=="__main__":
     main()
-
