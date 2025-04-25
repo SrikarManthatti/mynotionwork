adeee44eefd9b15c535b5eb0981af1d28e062862
Srikar Manthatti <srikarmanthatti@gmail.com>
2025-04-25 19:09:22 +1000

    moving load env to init instead of single py files

--- src/nuploaders/__init__.py
+++ src/nuploaders/__init__.py
@@ -5,3 +5,13 @@ logging.basicConfig(
     format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     datefmt = '%Y-%m-%d %H:%M:%S'
 )
+
+log = logging.getLogger(__name__)
+
+try:
+    from dotenv import load_dotenv
+except ModuelNotFoundError as me:
+    log.error("python-dotenv module not found; skipping .env loading.")
+else:
+    load_dotenv()
+    log.info(".env file loaded successfully.")
