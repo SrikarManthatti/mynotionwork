99e3478d376900fa79de3aa04da2af7f986bb61c
Srikar Manthatti <srikarmanthatti@gmail.com>
2025-04-27 17:00:04 +1000

    making omdb as async

--- nuploaders/imdb_url_fetcher.py
+++ nuploaders/imdb_url_fetcher.py
@@ -1,4 +1,4 @@
-import requests
+import httpx
 import os
 from typing import Optional #for type hinting
 from dataclasses import dataclass
@@ -12,14 +12,15 @@ class IMDBFetcher(): #make a cache folder to load url quickly instead of hitting
         self.headers = {'User-Agent': 'Mozilla/5.0'} #using it to mimic browser, else IMDB return 403 client error
         self.ref = "?ref_=fn_all_ttl_1"
     
-    def generate_link(self, titleid: str) -> str:
-        log.info("Validating URL for titleid: %s", titleid)
-        return self._validate_imdb_url(f"{self.base_link}/title/{titleid}/{self.ref}")
+    async def generate_link(self, titleid: str) -> str:
+            log.info("Validating URL for titleid: %s", titleid)
+            return await self._validate_imdb_url(f"{self.base_link}/title/{titleid}/{self.ref}")
     
-    def _validate_imdb_url(self, link: str) -> str:
-        response = requests.get(link, headers = self.headers)
-        response.raise_for_status()
-        return link
+    async def _validate_imdb_url(self, link: str) -> str:
+            async with httpx.AsyncClient() as client:
+                response = await client.get(link, headers = self.headers)
+                response.raise_for_status()
+            return link
 
 
 @dataclass
@@ -42,31 +43,32 @@ class OMDBClient():
         self.api_key = os.getenv("OMDB_API_KEY") #api_key
         self.base_link = base_link
     
-    def fetch_movie(self, movie_name, mycategory) -> Optional[Movie]:
-        params = {"t": movie_name, "apikey": self.api_key}
+    async def fetch_movie(self, movie_name, mycategory) -> Optional[Movie]:
+            params = {"t": movie_name, "apikey": self.api_key}
 
-        try:
-            response = requests.get(self.base_link, params = params, timeout = 10)
-            log.info("Response received for movie %s ", movie_name)
-            response.raise_for_status()
-        except requests.RequestException as re:
-            log.Error("Error in fetching information for movie: %s", movie_name)
-            return None
-        
-        imdb_obj = IMDBFetcher(CONFIG["imdb"]["base_link"]) #pass config value
-        data = response.json()
-        # check EXAMPLES at https://www.omdbapi.com/ to get sample response
-        if data.get("Response") == "True":
-            return Movie(
-                title = data.get("Title", "N/A"),
-                runtime = data.get("Runtime", "N/A"),
-                mycategory = mycategory,
-                genre =  data.get("Genre", "N/A"),
-                ratings =  data.get("Ratings", []), 
-                imdbrating =  data.get("imdbRating","N/A"),
-                imdbid =  data.get("imdbID","N/A"),
-                imdblink = imdb_obj.generate_link(data.get("imdbID","N/A")),
-            )
+            try:
+                async with httpx.AsyncClient() as asycclient:
+                    response = await asycclient.get(self.base_link, params = params, timeout = 10)
+                    log.info("Response received for movie %s ", movie_name)
+                response.raise_for_status()
+            except httpx.RequestError as re:
+                log.Error("Error in fetching information for movie: %s", movie_name)
+                return None
+
+            imdb_obj = IMDBFetcher(CONFIG["imdb"]["base_link"]) #pass config value
+            data = response.json()
+            # check EXAMPLES at https://www.omdbapi.com/ to get sample response
+            if data.get("Response") == "True":
+                return Movie(
+                    title = data.get("Title", "N/A"),
+                    runtime = data.get("Runtime", "N/A"),
+                    mycategory = mycategory,
+                    genre =  data.get("Genre", "N/A"),
+                    ratings =  data.get("Ratings", []), 
+                    imdbrating =  data.get("imdbRating","N/A"),
+                    imdbid =  data.get("imdbID","N/A"),
+                    imdblink = await imdb_obj.generate_link(data.get("imdbID","N/A")),
+                )
         
 
 
--- nuploaders/main.py
+++ nuploaders/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from nuploaders import CONFIG, log
 from nuploaders import file_reader, imdb_url_fetcher, notion_client
 import argparse
+import asyncio
 
 def main():
     #read the data file
@@ -16,7 +17,7 @@ def main():
     rdobj = file_reader.ReadActor()
     df_obj = rdobj.read_file(CONFIG["raw_files"]["movies"]["path"])
     df = df_obj.read()
-    cleaned_df = clean_movies_dataframe(df)
+    cleaned_df = asyncio.run(clean_movies_dataframe(df))
     notion_obj = notion_client.NotionDBManager()
 
     #notion db config
@@ -40,16 +41,22 @@ def main():
         for idx,row in cleaned_df.iterrows():
             notion_obj.write_to_database(row, db_name['id'])
 
-def clean_movies_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-    """THe sheet which we got have myowncategory as columns so we will change that as a column"""
-    #unpivoting the df to make column names as myown category
-    unpivot_df = df.melt(var_name='mycategory', value_name='name')
-    unpivot_df = unpivot_df[['name', 'mycategory']]
+async def clean_movies_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+        """THe sheet which we got have myowncategory as columns so we will change that as a column"""
+        #unpivoting the df to make column names as myown category
+        unpivot_df = df.melt(var_name='mycategory', value_name='name')
+        unpivot_df = unpivot_df[['name', 'mycategory']]
 
-    omdb_obj = imdb_url_fetcher.OMDBClient(CONFIG["omdb"]["base_link"])
-    for index, row in unpivot_df.iterrows():
+        omdb_obj = imdb_url_fetcher.OMDBClient(CONFIG["omdb"]["base_link"])
+        tasks = []
+        for index, row in unpivot_df.iterrows():
+            tasks.append(fetch_and_update(unpivot_df, omdb_obj, index, row))
+        await asyncio.gather(*tasks)
+        return unpivot_df
+
+async def fetch_and_update(unpivot_df, omdb_obj, index, row):
         log.info("Info for movie %s", row['name'])
-        movie_obj = omdb_obj.fetch_movie(row['name'], row['mycategory'])
+        movie_obj = await omdb_obj.fetch_movie(row['name'], row['mycategory'])
         unpivot_df.loc[index, ['title','runtime','enrich_mycategory','genre','ratings','imdbrating','imdbid','imdblink']] = [
         movie_obj.title,
         movie_obj.runtime,
@@ -59,8 +66,7 @@ def clean_movies_dataframe(df: pd.DataFrame) -> pd.DataFrame:
         movie_obj.imdbrating,
         movie_obj.imdbid,
         movie_obj.imdblink
-    ]
-    return unpivot_df
+        ]
 
 def get_col_properties_format(df):
     #{"cola":{"type":"string","col_options":[]}}
